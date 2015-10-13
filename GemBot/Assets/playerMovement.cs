4184053c6dd5c2daf872425205af62edf61d35af
Miles Meacham <littlemeacham@gmail.com>
2015-10-12 19:04:33 -0600

    Added swipe motions for the movement

--- GemBot/Assets/playerMovement.cs
+++ GemBot/Assets/playerMovement.cs
@@ -7,8 +7,9 @@ public class playerMovement : MonoBehaviour {
 
 	public int speed = 5;
 	public int jumpForce = 10;
+	public int moveVelocity = 10;
 
-	public float moveVelocity;
+	//public float moveVelocity;
 
 	public bool grounded;
 	public Transform groundCheck;
@@ -16,6 +17,10 @@ public class playerMovement : MonoBehaviour {
 	public float groundCheckRadius = 0.1f;
 
 	private Vector3 bodyUp;
+
+	public float minSwipeDistY;
+	public float minSwipeDistX;
+	private Vector2 startPos;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -24,12 +29,79 @@ public class playerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Jump ();
-		Movement ();
+
 	}
 
 	void FixedUpdate () {
+		Jump ();
+		SwipeMovement ();
+	}
 
+	void SwipeMovement () {
+		if (Input.touchCount > 0) 
+			
+		{
+			
+			Touch touch = Input.touches[0];
+			
+			
+			
+			switch (touch.phase) 
+				
+			{
+				
+			case TouchPhase.Began:
+				
+				startPos = touch.position;
+				
+				break;
+				
+				
+				
+			case TouchPhase.Ended:
+				
+				float swipeDistVertical = (new Vector3(0, touch.position.y, 0) - new Vector3(0, startPos.y, 0)).magnitude;
+				
+				if (swipeDistVertical > minSwipeDistY) 
+					
+				{
+					
+					float swipeValue = Mathf.Sign(touch.position.y - startPos.y);
+					
+					if (swipeValue > 0)//up swipe
+					{
+						//Jump ();
+						rb.velocity = transform.TransformDirection (new Vector3 (0, jumpForce, 0));
+					}
+					else if (swipeValue < 0)//down swipe
+					{
+						rb.velocity = transform.TransformDirection (new Vector3 (0, -jumpForce, 0));	
+						//Shrink ();
+					}		
+				}
+				
+				float swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+				
+				if (swipeDistHorizontal > minSwipeDistX) 
+					
+				{
+					
+					float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+					
+					if (swipeValue > 0)//right swipe
+					{
+						rb.velocity = transform.TransformDirection (new Vector3 (moveVelocity, 0, 0));
+						//MoveRight ();
+					}	
+					else if (swipeValue < 0)//left swipe
+					{
+							//MoveLeft ();
+						rb.velocity = (rb.position + transform.TransformDirection (-moveVelocity, 0, 0) * speed * Time.deltaTime);
+					}		
+				}
+				break;
+			}
+		}
 	}
 
 	void Movement () {
